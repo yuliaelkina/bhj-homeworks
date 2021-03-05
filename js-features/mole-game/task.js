6e7e02ce690bbaa14ddfe17978c756aa451ce51a
yulia elkina <todg@mail.ru>
2021-03-05 22:31:28 +0300

    mole_shooter done

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,37 @@
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+let lostMolesAmount;
+let deadMolesAmount;
+function getHole(ind) {
+  return document.getElementById(`hole${ind}`);
+};
+
+for (let ind = 1; ind <10; ind++) {
+  getHole(ind).onclick = shoot;
+};
+
+function shoot () {
+  if(this.classList.contains("hole_has-mole")) {
+  deadMolesAmount = parseInt(dead.textContent);
+    if (deadMolesAmount == 9) {
+      dead.textContent = 0;
+      lost.textContent = 0;
+      return alert("Вы гроза кротов!");
+    }
+    else {
+      dead.textContent = deadMolesAmount + 1;
+    }
+  }
+  else {
+    lostMolesAmount = parseInt(lost.textContent);
+    if (lostMolesAmount == 4) {
+      dead.textContent = 0;
+      lost.textContent = 0;
+      return alert("Повезет в следующий раз");
+    }
+    else {
+      lost.textContent = lostMolesAmount + 1;
+    }
+  };
+};
