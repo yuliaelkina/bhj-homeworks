8b55b5d3b3d3e336f713b9374cbdf4ba19dc00a6
yulia elkina <todg@mail.ru>
2021-03-11 23:01:13 +0300

    keysolo done

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+  document.addEventListener("keypress", (e) => {
+    if (e.key == this.currentSymbol.textContent) {
+      this.success();
+    }
+    else {
+      this.fail();
+    };
+  }) 
   }
 
   success() {
